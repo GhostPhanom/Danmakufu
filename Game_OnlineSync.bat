310b7ef642f5e076befb1690b416dd014b65dab0
GhostPhanom <theghostphanom@gmail.com>
2019-10-24 07:06:13 +0200

    AcroMage Update

---
+++ Game_OnlineSync.bat
@@ -0,0 +1,29 @@
+@echo off
+
+:a
+echo "Zum Beenden druecke STRG + C"
+timeout 6
+
+REM Updates remote_gamestate
+.\PSFTP.EXE thomasgordon@battleship-eclipse.de -P 61111 -pw "WieEsWohlSchmeckt" -b Game_Download.txt
+echo "Gamestate Downloaded"
+
+if exist "reset_gamestate" (
+del remote_gamestate
+del gamestate
+move reset_gamestate remote_gamestate
+.\PSFTP.EXE thomasgordon@battleship-eclipse.de -P 61111 -pw "WieEsWohlSchmeckt" -b Game_Upload.txt
+echo "GAMESTATE RESET UPLOADED!!!"
+) 
+if exist "gamestate" (
+del remote_gamestate
+move gamestate remote_gamestate
+.\PSFTP.EXE thomasgordon@battleship-eclipse.de -P 61111 -pw "WieEsWohlSchmeckt" -b Game_Upload.txt
+echo "GAMESTATE RESET UPLOADED!!!"
+) 
+
+
+goto :a
+timeout 6
+
+15:54
